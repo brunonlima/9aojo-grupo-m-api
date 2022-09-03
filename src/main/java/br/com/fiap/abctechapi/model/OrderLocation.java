aa3856812a89c09b02d89e30250f28ba45ce1da3
brunonlima <brunofatecscs@gmail.com>
2022-09-03 19:11:27 -0300

    Acrescentando classes de Order e OrderLocation

---
+++ src/main/java/br/com/fiap/abctechapi/model/OrderLocation.java
@@ -0,0 +1,24 @@
+package br.com.fiap.abctechapi.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+    private Date date;
+}
