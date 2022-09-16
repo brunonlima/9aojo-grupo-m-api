d83d538ace01844f4c228b16935d159074721ba4
brunonlima <brunofatecscs@gmail.com>
2022-09-16 00:00:51 -0300

    Adicionando Exceptions

--- src/main/java/br/com/fiap/abctechapi/model/Order.java
+++ src/main/java/br/com/fiap/abctechapi/model/Order.java
@@ -1,19 +1,20 @@
 package br.com.fiap.abctechapi.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
-
 @Entity
 @Table(name = "orders")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -23,15 +24,22 @@ public class Order {
     private Long operatorId;
 
     @ManyToMany
-    private List<Assistance> services;
+    private List<Assistance> assists;
 
     @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name =  "start_order_location_id", foreignKey = @ForeignKey(name = "FK_start_order_id"))
+    @JoinColumn(name = "start_order_location_id", foreignKey = @ForeignKey(name ="FK_start_order_id"))
     private OrderLocation startOrderLocation;
 
     @OneToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name =  "end_order_location_id", foreignKey = @ForeignKey(name = "FK_end_order_id"))
+    @JoinColumn(name = "end_order_location_id" , foreignKey = @ForeignKey(name ="FK_end_order_id"))
     private OrderLocation endOrderLocation;
 
 
+    public boolean hasMinAssists (){
+        return assists.size() > 0;
+    }
+
+    public boolean exceedsMaxAssists () {
+        return assists.size() > 15;
+    }
 }
--- src/test/java/br/com/fiap/abctechapi/service/AssistanceServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/AssistanceServiceTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 
+
 @SpringBootTest
 public class AssistanceServiceTest {
 
@@ -25,26 +26,25 @@ public class AssistanceServiceTest {
         assistanceService = new AssistanceServiceImpl(assistanceRepository);
     }
 
-    @DisplayName("Listando assistencias disponiveis :: sucesso")
+    @DisplayName("Listando assistencias disponiveis :: Sucesso")
     @Test
-    public void list_success() {
+    public void list_success(){
         Assistance assistance1 = new Assistance(1L, "Mock Assistance 1", "Description 1");
         Assistance assistance3 = new Assistance(3L, "Mock Assistance 3", "Description 3");
         Assistance assistance2 = new Assistance(2L, "Mock Assistance 2", "Description 2");
 
-        when(assistanceRepository.findAll()).thenReturn(List.of(assistance1,assistance2,assistance3));
+        when(assistanceRepository.findAll()).thenReturn(List.of(assistance1, assistance2, assistance3));
 
         List<Assistance> values = assistanceService.getAssistanceList();
 
-        Assertions.assertEquals(3,values.size());
+        Assertions.assertEquals(3, values.size());
         Assertions.assertSame(values.get(0), assistance1);
         Assertions.assertSame(values.get(1), assistance2);
     }
 
-
     @DisplayName("Listando assistencias indisponiveis :: Erro")
     @Test
-    public void list_error() {
+    public void list_error(){
         when(assistanceRepository.findAll()).thenReturn(List.of());
 
         List<Assistance> values = assistanceService.getAssistanceList();
@@ -52,3 +52,24 @@ public class AssistanceServiceTest {
     }
 
 }
+
+/**
+ *
+ *
+ * Order (id:Long, operator_id:Long, assistances: List<Assistance>, start:OrderLocation, end:OrderLocation) -> Orders
+ * OrderLocation(id:Long, latitude: Double, longitude: Double, date: Date) -> OrdersLocations
+ * {
+ * "operator_id" : 0,
+ * "services" : [1 , 2, 3],
+ * "start" : {
+ * 	"latitude" : 0.0,
+ * 	"longitude" : 0.0,
+ * 	"datetime" : "2022-02-19 00:00:00"
+ * },
+ * "end" : {
+ * 	"latitude" : 0.0,
+ * 	"longitude" : 0.0,
+ * 	"datetime" : "2022-02-19 00:00:00"
+ * }
+ * }
+ */
